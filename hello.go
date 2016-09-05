e248bd10775ae5ea092fa2eb6c49dea3db0fa15f
David Widen <david.widen1@gmail.com>
2016-09-05 18:55:23 -0400

    used a basic module, some lotr stuff

--- hello.go
+++ hello.go
@@ -1,8 +1,11 @@
 package main
 
-import "fmt"
+import ("fmt"
+        "github.com/dwiden/stringstuff"
+)
 
 // this process is weird, but thats okay
 func main() {
-    fmt.Printf("hello, world\n")
+    fmt.Printf(stringstuff.Reverse(stringstuff.Reverse("hello, world\n")))
+    fmt.Printf(stringstuff.Reverse("\nfladnaG-\t\n.su ot nevig si taht emit eht htiw od ot tahw si ediced ot evah ew llA"))
 }
